ba76bc804836c73765002530669accd648ab8978
Dima <67710847+inetbit24@users.noreply.github.com>
2022-03-02 10:24:52 +0300

    Delete

--- assets/js/vue/modal-buttons.vue
+++
@@ -1,7 +0,0 @@
-<script>
-define(['Vue', 'apolloProvider', 'gql','Forms'], function(Vue, apolloProvider, gql, forms) {
-
-    forms.init_modals_buttons_and_forms(Vue, apolloProvider, gql);
-
-});
-</script>
